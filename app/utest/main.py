11a739325f42f9291b023ef7a40b643e93957215
ROJAS Hector <hector.rojas@alstomgroup.com>
2020-04-28 08:10:30 -0400

    updated test example

--- app/utest/main.py
+++ app/utest/main.py
@@ -1,22 +1,7 @@
-import numpy as np
-import pandas as pd
-
-from bokeh.plotting import figure
-from bokeh.models import ColumnDataSource, CustomJS, Select, Range1d
-from bokeh.io import show, output_file, save
-from bokeh.layouts import row
-
-x = [0, 2, 4]
-y1 = [4, np.nan, 0]
-y2 = [8, 20, np.nan]
-
-import numpy as np
-import pandas as pd
-
 from bokeh.plotting import figure
 from bokeh.models import ColumnDataSource, Select
 from bokeh.io import curdoc
-from bokeh.layouts import row, column
+from bokeh.layouts import column
 
 x = [0, 2, 4]
 y1 = [4, 10, 20]
@@ -32,11 +17,11 @@ lines['line2'] = p.line('x', 'y2', source=source, visible=True)
 
 select = Select(value='line1', options=['line1', 'line2'])
 
-def update_plot(attr, old, new):
+def update(attr, old, new):
     lines[old].visible = not lines[old].visible
     lines[new].visible = not lines[new].visible
 
-select.on_change('value', update_plot)
+select.on_change('value', update)
 
 # add to document
 curdoc().add_root(column(select, p))
