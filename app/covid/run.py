807afa0d875b807763055e80b1e191709a8bb443
ROJAS Hector <hector.rojas@alstomgroup.com>
2020-05-12 17:21:28 -0400

    host setting

--- app/covid/run.py
+++ app/covid/run.py
@@ -6,4 +6,4 @@ from waitress import serve
 from app import app
 
 print(f'port number {int(os.environ.get("PORT", 8000))}')
-serve(app, host='0.0.0.0', port=int(os.environ.get("PORT", 8000)))
+serve(app, port=int(os.environ.get("PORT", 8000)))
