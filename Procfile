b9d2ca300ad1d798912d0fa150d6c6846f857753
ROJAS Hector <hector.rojas@alstomgroup.com>
2020-05-14 18:14:13 -0400

    test6

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: bokeh serve --port=$PORT --allow-websocket-origin=safe-scrubland-67589.herokuapp.com --address=0.0.0.0 --use-xheaders app/covid
+web: 
--- app/utest/app.py
+++ app/utest/app.py
@@ -16,17 +16,17 @@ except ImportError:
 
 from threading import Thread
 
+from waitress import serve
+
 from flask import Flask, render_template
 from flask_cors import CORS
 
 from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 
-from bokeh import __version__
-from bokeh.resources import get_sri_hashes_for_version
 from bokeh.application import Application
 from bokeh.application.handlers import FunctionHandler
-from bokeh.embed import server_document, components
+from bokeh.embed import server_document
 from bokeh.models import ColumnDataSource
 from bokeh.plotting import figure
 from bokeh.sampledata.sea_surface_temperature import sea_surface_temperature
@@ -35,37 +35,25 @@ from bokeh.server.tornado import BokehTornado
 from bokeh.server.util import bind_sockets
 
 
+LOCAL_TESTING = True
+
+
 app = Flask(__name__)
+
 
 app.config['CORS_HEADERS'] = 'Content-Type'
 cors = CORS(app)
 
-# get get_sri_hashes_for_version(__version__)
 
-def cwd():
-    """Return current working directory if running from bokeh server,
-       jupiter or python.
-
-    Returns:
-        String -- path to current working directory
-    """
-    try:
-        __file__
-    except NameError:
-        working_dir = os.getcwd()
-    else:
-        working_dir = os.path.dirname(__file__)
-    return working_dir
+def get_host():
+    host = 'safe-scrubland-67589.herokuapp.com'
+    if LOCAL_TESTING:
+        host = '127.0.0.1'
+    return host
 
 
-# get heroku app name in .env file
-with open(join(cwd() + '/.env'), 'r') as env_file:
-    app_name = env_file.read()
-
-if not re.match(r'HEROKU_APP_NAME', app_name):
-    sys.exit('ERROR: add HEROKU_APP_NAME to .env file')
-else:
-    app_name = re.sub(r'HEROKU_APP_NAME\s{0,}=', '', app_name).strip()
+def get_port():
+    return int(os.environ.get('PORT', default='8000'))
 
 
 def bkapp(doc):
@@ -87,28 +75,12 @@ def bkapp(doc):
     return doc.add_root(plot)
 
 
-def graph_plot():
-    """ Test plot with no callbacks, no controls
-
-    Returns:
-        [type] -- [description]
-    """
-    dataframe = sea_surface_temperature.copy()
-    source = ColumnDataSource(data=dataframe)
-
-    plot = figure(x_axis_type='datetime', y_range=(0, 25),
-                  y_axis_label='Temperature (Celsius)',
-                  title="Sea Surface Temperature at 43.18, -70.43")
-    plot.line(x='time', y='temperature', source=source)
-    return plot
-
-
 # can't use shortcuts here, since we are passing to low level BokehTornado
 bkapp = Application(FunctionHandler(bkapp))
 
 
 # each process will listen on its own port
-sockets, port = bind_sockets('0.0.0.0', 0)
+sockets, port = bind_sockets(get_host(), 0)
 
 
 @app.route('/')
@@ -118,15 +90,13 @@ def index():
 
 @app.route('/graph', methods=['GET'])
 def graph_route():
-    script = server_document(f"https://{app_name}:{port}/bkapp",
-                             resources=None)
+    script = server_document(f"http://{get_host()}:{port}/bkapp")
     return render_template("embed.html", script=script, framework="Flask")
 
 
-@app.route('/plot', methods=['GET'])
-def plot_route():
-    script, div = components(graph_plot())
-    return render_template("embed.html", div=div, script=script, framework="Flask")
+@app.route('/port', methods=['GET'])
+def port_route():
+    return str(sockets) + str(port)
 
 
 @app.route('/env', methods=['GET'])
@@ -134,6 +104,8 @@ def env_route():
     env = '<p>'
     for  key, value in os.environ.items():
         env += f"<br>{key}: {value}"
+
+    print('printing to browser', file=sys.stderr)
     return env + '</p>'
 
 
@@ -141,25 +113,41 @@ def bk_worker():
     """ Worker thread to run the bokeh server once, so Bokeh document can be
         extracted for every http request.
     """
+    print(f'bokeh server listening at port={port}', file=sys.stderr)
+
     asyncio.set_event_loop(asyncio.new_event_loop())
 
-    env_port = os.environ.get('PORT', default='8000')
-
-    websocket_origins = ["0.0.0.0", app_name, 'localhost']
+    websocket_origins = [f"{get_host()}:{port}",
+                         f"{get_host()}:{get_port()}",
+                         f"localhost:{port}",
+                         f"localhost:{get_port()}"
+                         f"127.0.0.1:{port}",
+                         f"127.0.0.1:{get_port()}"]
 
     conf = {'use_xheaders': True}
     bokeh_tornado = BokehTornado({'/bkapp': bkapp},
                                  extra_websocket_origins=websocket_origins,
                                  **conf)
 
-
-    bokeh_http = HTTPServer(bokeh_tornado, xheaders=True)
+    bokeh_http = HTTPServer(bokeh_tornado, xheaders=True, protocol='https')
     bokeh_http.add_sockets(sockets)
 
     server = BaseServer(IOLoop.current(), bokeh_tornado, bokeh_http)
     server.start()
     server.io_loop.start()
 
+
 t = Thread(target=bk_worker)
 t.daemon = True
 t.start()
+
+if __name__ == '__main__':
+    print(f"main server listening at {get_host()}:{get_port()}", file=sys.stderr)
+    serve(app,
+          threads=4,
+          host='*',
+          port=get_port(),
+          channel_timeout=660,
+          trusted_proxy=get_host(),
+          trusted_proxy_headers="x-forwarded-for x-forwarded-host x-forwarded-proto x-forwarded-port",
+          log_untrusted_proxy_headers=True)
